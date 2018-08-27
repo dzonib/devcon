a272ab36775bab1affdbefe66b268e5fa9ec687e
dzonib <nikolabosnjak381@gmail.com>
2018-08-27 20:31:47 +0200

    implemented react router, added state to register component

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,24 @@ import './App.css';
 import Navbar from './components/Layout/Navbar';
 import Footer from './components/Layout/Footer';
 import Landing from './components/Layout/Landing';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Navbar />
-      <Landing/>
-      <Footer/>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing}/>
+          <div className="container">
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
