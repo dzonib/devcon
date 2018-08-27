53553c598bc31d21cea75285110babfd6a22994e
dzonib <nikolabosnjak381@gmail.com>
2018-08-27 14:37:22 +0200

    added footer header and landing page

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './components/Layout/Navbar';
+import Footer from './components/Layout/Footer';
+import Landing from './components/Layout/Landing';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Navbar />
+      <Landing/>
+      <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
